d3b63c37209cf7d9382c16d44e04f6e68ee92a88
Philip A Chapman <pchapman@pcsw.us>
2018-10-24 12:24:20 -0700

    Added ability to mark processqueue items as errored.

--- processQueue.go
+++ processQueue.go
@@ -19,12 +19,14 @@ type ProcessQueueServiceClient struct {
 	env *Environment
 }
 
+const paramError = "error"
 const paramProcessor = "processor"
 const paramProcessType = "processType"
 
 const pqRootURI = "%s/processQueue"
 const pqClaimURI = pqRootURI + "/claim?%s"
 const pqEnqueueURI = pqRootURI + "/enqueue"
+const pqErroredURI = pqRootURI + "/%s/errored/%s"
 const pqProcessedURI = pqRootURI + "/processed"
 
 
@@ -39,12 +41,20 @@ func (client ProcessQueueServiceClient) Claim(processorId string, processType st
 }
 
 func (client ProcessQueueServiceClient) Enqueue(entries []ProcessQueueEntry) error {
-	data, err := json.Marshal(entries);
+	data, err := json.Marshal(entries)
 	if err != nil {
 		return err
 	}
 	url := fmt.Sprintf(pqEnqueueURI, client.env.ServiceURI)
 	err = executeRestCall(client.env, "POST", url, data, nil)
+	return err
+}
+
+func (client ProcessQueueServiceClient) MarkErrored(entryId string, error string) error {
+	v := url.Values{}
+	v.Add(paramError, error)
+	url := fmt.Sprintf(pqErroredURI, client.env.ServiceURI, entryId, v.Encode())
+	err := executeRestCall(client.env, "POST", url, nil, nil)
 	return err
 }
 
